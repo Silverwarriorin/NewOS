7058a9e977ad3fdef8a7c37882b9af04a34f6705
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2021-01-09 23:07:19 -0500

    Initial Commit

---
+++ NewOS.asm
@@ -0,0 +1,381 @@
+;;Program start at 0000h
+
+org 0000h;
+
+;;Init of the serial port at 9600 baud
+;;Can only be called once per reset
+
+init_port:  ld a,04eh
+
+    out (3),a
+    ld a,037h
+    out (3),a 
+    ret 
+
+;;Routine to print a character
+
+print_char:  ld b,a 
+
+print_char_loop:  in a,(3)
+
+    and 001h
+    jp z,print_char_loop
+    ld a,b
+    out (2),a 
+    ret
+
+;;
+;;Routine to print a string
+;;
+
+print_string:  in a,(3)
+
+    and 001h
+    jp z,print_string
+    ld a,(hl)
+    and a 
+    ret z 
+    out (2),a 
+    inc hl
+    jp print_string
+
+;;
+;;load file from port
+;;
+
+load_file:  in a,(3)
+
+    and 002h
+    jp z,load_file
+    in a,(2)
+    ld (hl),a
+    inc hl
+    dec bc 
+    ld a,b 
+    or c 
+    jp nz,load_file
+    ret
+
+;;
+;;Press any key routine
+;;
+
+press_enter:  in a,(3)
+
+    and 002h
+    jp z,press_enter
+    in a,(2)
+    cp 00dh
+    ret z
+
+;;
+;;Clear routines
+;;
+
+clear_display:  ld b,019h
+
+    
+    call clear_display_2
+    
+clear_display_2:  call newline    
+
+    djnz clear_display
+    ret
+
+;;
+;;Routines to print multiple spaces NOTE: load a value into the b register prior to calling
+;;
+
+jump_spaces_1:  ld a,020h 
+
+    call jump_spaces_2
+
+jump_spaces_2:  call print_char
+
+    djnz jump_spaces_2
+    ret
+
+;;
+;;Write Newline
+;;
+
+newline:  ld a,00ah
+
+    call print_char
+
+;;
+;;All the startup message routines
+;;
+
+startup_messages:  ld hl,Top_Bar_Color
+
+    call print_string
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,00ah
+    call jump_spaces_1
+
+    ld hl,Welcome_Message
+    call print_string
+    ld b,00ah
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,00ah
+    call jump_spaces_1
+
+    ld hl,Please_Enter
+    call print_string
+    ld b,00ah
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,014h
+    call jump_spaces_1
+    ld hl,Window_Color
+    call print_string
+    ld b,028h
+    call jump_spaces_1
+
+    ld hl,Main_Panel_Color
+    call print_string
+    ld b,014h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    call newline
+
+    ld b,050h
+    call jump_spaces_1
+
+    ret
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;
+;; Colors
+;;
+
+Top_Bar_Color:  defm "\033[48;5;250m",0
+Main_Panel_Color:  defm "\033[48;5;255m",0
+Window_Color:  defm "\033[48;5;7m",0
+Text:  defm "\033[38;5;0m",0
+
+;;
+;;Messages
+;;
+
+Welcome_Message: defm "Welcome to my new OS",0
+Please_Enter: defm "[PLEASE PRESS ENTER]",0
+Tada: defm "Tada",0
+
+;;
+;; Stuff to execute
+;;
+
+cold_start:  ld sp,ROM_monitor
+
+    call init_port
+    ld hl,Text
+    call print_string
+    call startup_messages
+    call press_enter
+    call clear_display
+    ld hl,Tada
+    call print_string
+    
+
+;;
+;;call after software reset
+;;
+
+warm_start: call clear_display
+
